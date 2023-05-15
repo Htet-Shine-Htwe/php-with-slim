548afb1bf2305674c420e70a4818029b6aa44c0f
htetshine <htetshine.htetmkk@gmail.com>
2023-05-15 22:34:58 +0630

    add Auth,migration and register logic

--- migrations/Version20230515074438.php
+++ migrations/Version20230515074438.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230515041700 extends AbstractMigration
+final class Version20230515074438 extends AbstractMigration
 {
     public function getDescription(): string
     {
