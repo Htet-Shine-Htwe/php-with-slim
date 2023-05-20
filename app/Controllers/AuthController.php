a3998199d9f9438822c09e87cc9eeda70a8df115
htet shine <htetshine.coin@gmail.com>
2023-05-20 22:54:09 +0630

    push some

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -3,42 +3,60 @@
 namespace App\Controllers;
 
 use App\Entity\User;
+use App\Exceptions\ValidationException;
 use Doctrine\ORM\EntityManager;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\Twig;
 use Psr\Http\Message\ServerRequestInterface as Request;
-
+use Valitron\Validator;
 
 class AuthController
 {
-    public function __construct(private readonly Twig $twig,private readonly EntityManager $entityManager)
+    public function __construct(private readonly Twig $twig, private readonly EntityManager $entityManager)
     {
 
     }
 
-    public function loginView(Request $request,Response $response):Response
+    public function loginView(Request $request, Response $response): Response
     {
-        return $this->twig->render($response,'auth/login.twig');
+        return $this->twig->render($response, 'auth/login.twig');
     }
 
-    public function registerView(Request $request,Response $response):Response
+    public function registerView(Request $request, Response $response): Response
     {
-        return $this->twig->render($response,'auth/register.twig');
+        return $this->twig->render($response, 'auth/register.twig');
     }
 
-    public function login(Request $request,Response $response):Response
+    public function login(Request $request, Response $response): Response
     {
         return $response;
     }
 
-    public function register(Request $request,Response $response):Response
+    public function register(Request $request, Response $response): Response
     {
-        $data =  $request->getParsedBody(); 
-
+        $data = $request->getParsedBody();
+        $v = new Validator($data);
+        $v->rule('required', ['name', 'email','password','confirmPassword']);
+        $v->rule('email', 'email');
+        $v->rule('equals', 'confirmPassword','password')->label('Confirm Password');
+        $v->rule(
+            fn($field,$value,$params,$fields)=> !$this->entityManager->getRepository(User::class)->count(
+                ['email' => $value]
+            ),'email'
+        )->message('User with this email is already exists');
+        if ($v->validate())
+        {
+            echo "Yay! We're all good!";
+        }
+        else
+        {
+            throw new ValidationException(errors :$v->errors());
+        }
+        exit;
         $user = new User();
         $user->setName($data['name']);
         $user->setEmail($data['email']);
-        $user->setPassword(password_hash($data['password'],PASSWORD_BCRYPT,['cost' => 12]));
+        $user->setPassword(password_hash($data['password'], PASSWORD_BCRYPT, ['cost' => 12]));
 
         $this->entityManager->persist($user);
         $this->entityManager->flush();
